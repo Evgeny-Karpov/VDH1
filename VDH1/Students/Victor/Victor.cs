4ed228334b58639b71f34e9a6c98a406f231291a
Victor Andkilde <victor.andkilde@gmail.com>
2021-06-03 09:01:57 +0200

    Victor

---
+++ VDH1/Students/Victor/Victor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDH1.Students.Victor
+{
+    class Victor
+    {
+        public string Fullname { get => "Victor Andkilde"; }
+    }
+}
