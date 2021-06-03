7ba38cfc3c6c41efd03c42709e3333ccfe732ce1
evgeny.karpov <evgeny.karpov@visma.com>
2021-06-03 09:11:29 +0200

    Added Evgeny

---
+++ VDH1/Students/Evgeny/Evgeny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDH1.Students.Evgeny
+{
+    class Evgeny
+    {
+        private string _fname = "Evgeny";
+        private string _lname = "Karpov";
+
+
+        public string GetFullName()
+        {
+            return String.Concat(_fname, " ", _lname);
+        }
+    }
+}
